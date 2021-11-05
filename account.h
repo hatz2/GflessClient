53c072504c11b3af3f1fc5af45f62f3a3513f56f
hatz02 <alextorres4101@gmail.com>
2021-11-05 21:03:27 +0100

    Initial commit

---
+++ account.h
@@ -0,0 +1,38 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include "profile.h"
+#include "nostaleauth.h"
+#include <QObject>
+#include <QList>
+
+class Account : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Account(QString gameforgeAccountUsername, QObject *parent = nullptr);
+
+    void addRawAccount(const QString& displayName, const QString& id);
+
+    void addProfile(Profile* profile);
+
+    void removeProfile(int i);
+
+    const QString &getGameforgeAccountUsername() const;
+
+    const QList<Profile *> &getProfiles() const;
+
+    const QMap<QString, QString> &getAccounts() const;
+
+    void setAuthenticator(NostaleAuth *newAuthenticator);
+
+    NostaleAuth *getAuthenticator() const;
+
+private:
+    QString gameforgeAccountUsername;
+    NostaleAuth* authenticator;
+    QMap<QString /* display name */, QString /* id */> accounts; // Raw accounts
+    QList<Profile*> profiles;
+};
+
+#endif // ACCOUNT_H
