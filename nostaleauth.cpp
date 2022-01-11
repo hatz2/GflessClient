7c3ac916e07bffabd4e617508896e03f626d521e
Are <alextorres4101@gmail.com>
2022-01-11 20:05:21 +0100

    Updated gf version

--- nostaleauth.cpp
+++ nostaleauth.cpp
@@ -3,8 +3,8 @@
 NostaleAuth::NostaleAuth(QObject *parent) : QObject(parent)
 {
     this->locale = "en_GB";
-    this->chromeVersion = "C2.2.19.1700";
-    this->gameforgeVersion = "2.2.19";
+    this->chromeVersion = "C2.2.22.1801";
+    this->gameforgeVersion = "2.2.22";
     this->browserUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36";
 
     initCert();
