e1c72eee793861f1d61b88e2eeb606c21629a65a
Hatz <alextorres4101@gmail.com>
2022-09-12 11:10:42 +0200

    Removed hardcoded locale, added dynamic locale

--- nostaleauth.cpp
+++ nostaleauth.cpp
@@ -2,7 +2,7 @@
 
 NostaleAuth::NostaleAuth(const std::shared_ptr<Identity> &id, QObject *parent) : QObject(parent), identity(id)
 {
-    this->locale = "en_GB";
+    this->locale = QLocale().name();
     this->chromeVersion = "C2.2.23.1813";
     this->gameforgeVersion = "2.2.23";
     this->browserUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36";
