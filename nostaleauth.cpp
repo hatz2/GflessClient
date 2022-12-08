3978b319cd414ccea3802b3010e586b4b9424f47
Hatz <alextorres4101@gmail.com>
2022-12-08 15:20:49 +0100

    Updated gameforge client version

--- nostaleauth.cpp
+++ nostaleauth.cpp
@@ -3,8 +3,8 @@
 NostaleAuth::NostaleAuth(const std::shared_ptr<Identity> &id, QObject *parent) : QObject(parent), identity(id)
 {
     this->locale = QLocale().name();
-    this->chromeVersion = "C2.2.23.1813";
-    this->gameforgeVersion = "2.2.23";
+    this->chromeVersion = "C2.2.27.1832";
+    this->gameforgeVersion = "2.2.27";
     this->browserUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36";
 
     initCert();
