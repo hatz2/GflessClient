1d328144b2ffa443aaf7e471891b11dba5199788
Hatz <alextorres4101@gmail.com>
2023-05-17 10:43:27 +0200

    Added some comments and exit code check

--- src/injector.h
+++ src/injector.h
@@ -9,12 +9,15 @@ bool Inject(DWORD pid, const char* dllPath)
     HANDLE hProc;
     HANDLE hThread;
     LPVOID lpAllocMem;
+    DWORD exitCode;
 
+    // Retrieve a handle to the game process
     hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
     if (!hProc)
         return false;
 
+    // Allocate memory in the target process to write the DLL path
     lpAllocMem =  VirtualAllocEx(hProc, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 
     if (!lpAllocMem)
@@ -24,6 +27,7 @@ bool Inject(DWORD pid, const char* dllPath)
         return false;
     }
 
+    // Write the DLL path into the allocated memory
     if (!WriteProcessMemory(hProc, lpAllocMem, dllPath, strlen(dllPath) + 1, nullptr))
     {
         qDebug() << "Error on WriteProcessMemory:" << GetLastError();
@@ -31,6 +35,7 @@ bool Inject(DWORD pid, const char* dllPath)
         return false;
     }
 
+    // Create a thread in the target memory that calls LoadLibraryA with the path to the DLL
     hThread = CreateRemoteThread(hProc, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, lpAllocMem, 0, NULL);
 
     if (!hThread)
@@ -46,6 +51,7 @@ bool Inject(DWORD pid, const char* dllPath)
         return false;
     }
 
+    // Waits for thread execution to finish
     if (WaitForSingleObject(hThread, INFINITE) == WAIT_FAILED)
     {
         qDebug() << "Error on WaitForSingleObject:" << GetLastError();
@@ -60,6 +66,37 @@ bool Inject(DWORD pid, const char* dllPath)
         return false;
     }
 
+    // Retrieve the exit code of the thread
+    if (!GetExitCodeThread(hThread, &exitCode))
+    {
+        qDebug() << "Error on GetExitCodeThread:" << GetLastError();
+
+        if (!VirtualFreeEx(hProc, lpAllocMem, 0, MEM_RELEASE))
+        {
+            qDebug() << "Error on VirtualFreeEx:" << GetLastError();
+        }
+
+        CloseHandle(hThread);
+        CloseHandle(hProc);
+        return false;
+    }
+
+    // Check if there was any error during the execution of LoadLibraryA in the target process
+    if (!exitCode)
+    {
+        qDebug() << "Error in the execution of LoadLibraryA in the target process:" << exitCode;
+
+        if (!VirtualFreeEx(hProc, lpAllocMem, 0, MEM_RELEASE))
+        {
+            qDebug() << "Error on VirtualFreeEx:" << GetLastError();
+        }
+
+        CloseHandle(hThread);
+        CloseHandle(hProc);
+        return false;
+    }
+
+    // Frees the allocated memory and close the process and thread handles
     if (!VirtualFreeEx(hProc, lpAllocMem, 0, MEM_RELEASE))
     {
         qDebug() << "Error on VirtualFreeEx:" << GetLastError();
